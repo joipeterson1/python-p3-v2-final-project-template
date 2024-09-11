e17c09ebed04cb970da8c8e6bb0d1ba96aecab69
Dalecia <petersondalecia@gmail.com>
2024-09-11 11:07:09 -0400

    added chores by id on cli

--- lib/cli.py
+++ lib/cli.py
@@ -4,6 +4,7 @@ from helpers import (
     exit_program, 
     list_chores, 
     find_chores_by_name, 
+    find_chores_by_id,
     create_chore, 
     update_chore, 
     delete_chore, 
@@ -29,28 +30,30 @@ def main():
         elif choice == "2":
             find_chores_by_name()
         elif choice == "3":
-            create_chore()
+            find_chores_by_id()
         elif choice == "4":
-            update_chore()
+            create_chore()
         elif choice == "5":
-            delete_chore()
+            update_chore()
         elif choice == "6":
-            list_house_members()
+            delete_chore()
         elif choice == "7":
-            find_house_member_by_name()
+            list_house_members()
         elif choice == "8":
-            find_house_member_by_id()
+            find_house_member_by_name()
         elif choice == "9":
-            create_house_member()
+            find_house_member_by_id()
         elif choice == "10":
-            update_house_member()
+            create_house_member()
         elif choice == "11":
-            delete_house_member()
+            update_house_member()
         elif choice == "12":
-            list_chore_by_schedule()
+            delete_house_member()
         elif choice == "13":
-            list_chore_by_location()
+            list_chore_by_schedule()
         elif choice == "14":
+            list_chore_by_location()
+        elif choice == "15":
             list_chore_house_member()
         else:
             print("Invalid choice")
@@ -61,18 +64,19 @@ def menu():
     print("0. Exit")
     print("1. List all chores")
     print("2. Find chores by name")
-    print("3. Create a new chore")
-    print("4. Update a chore")
-    print("5. Delete a chore")
-    print("6. List all House Members")
-    print("7. Find House Members by name")
-    print("8. Find House Members by id")
-    print("9. Create a new House Member")
-    print("10. Update a House Member")
-    print("11. Delete a House Member")
-    print("12. List all chores by schedule")
-    print("13. List all chores by location")
-    print("14. List all House Members per chore")
+    print("3. Find chores by ID")
+    print("4. Create a new chore")
+    print("5. Update a chore")
+    print("6. Delete a chore")
+    print("7. List all House Members")
+    print("8. Find House Members by name")
+    print("9. Find House Members by id")
+    print("10. Create a new House Member")
+    print("11. Update a House Member")
+    print("12. Delete a House Member")
+    print("13. List all chores by schedule")
+    print("14. List all chores by location")
+    print("15. List all House Members per chore")
 
 
 if __name__ == "__main__":
